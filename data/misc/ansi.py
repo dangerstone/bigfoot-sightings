b8317675c76c84883e5d4a1730bb4d70cc57b0e4
Stinna Danger <stinna.danger@lego.com>
2022-10-10 16:38:05 +0200

    added and implemented ansi table for counties/states

---
+++ data/misc/ansi.py
@@ -0,0 +1,81 @@
+import pandas as pd
+
+us_state_to_abbrev = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+    "Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR",
+    "United States Minor Outlying Islands": "UM",
+    "U.S. Virgin Islands": "VI",
+}
+
+# invert the dictionary
+abbrev_to_us_state = dict(map(reversed, us_state_to_abbrev.items()))
+
+df = pd.read_csv("data/misc/counties.csv")
+
+df.pop("idk")
+
+df["county_ansi_code"] = df["state_ansi_code"].astype(str).str.zfill(2) + df[
+    "county_ansi_code"
+].astype(str).str.zfill(3)
+df["state"] = [abbrev_to_us_state[x] for x in df["state_code"]]
+df["state_ansi_code"] = df["state_ansi_code"].astype(str).str.zfill(2)
+
+df = df.reindex(
+    columns=["state_code", "state", "county", "state_ansi_code", "county_ansi_code"]
+)
+
+
+df.to_csv("data/misc/ansi.csv", index=False)
